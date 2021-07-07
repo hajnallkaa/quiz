977a2990c871136a6e86a51106943f7b8771687f
hajnallkaa <hajnallkaa@gmail.com>
2021-07-07 09:11:48 +0200

    Added:quoz

---
+++ src/App.js
@@ -0,0 +1,64 @@
+
+import React, {useState, useEffect} from 'react';
+import {Questionaire} from './components';
+
+const API_URL = 'https://opentdb.com/api.php?amount=10&category=11&difficulty=easy';
+
+
+function App() {
+
+  const [questions, setQuestions] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showAnswers, setShowAnswers] = useState(false);
+
+
+  useEffect(() => {
+    fetch(API_URL)
+    .then((res) => res.json())
+    .then((data) => {
+      setQuestions(data.results);
+      const questions = data.results.map((question) => 
+      ({
+        ...question,
+        answers: [
+          question.correct_answer,
+          ...question.incorrect_answers,
+        ].sort(()=> Math.random()-0.5)
+      }));
+      setQuestions(questions);
+    });
+  }, []);
+
+  const handleAnswer = (answer) => {
+    
+    if(!showAnswers){
+    if(answer === questions[currentIndex].correct_answer){
+      setScore(score + 1);
+    }
+  }
+    setShowAnswers(true);
+  };
+
+  const handleNextQuestion = () => {
+    
+    setCurrentIndex(currentIndex + 1)
+    setShowAnswers(false);
+  };
+
+
+  return  questions.length > 0 ? (
+    <div className="container">
+      {currentIndex >= questions.length ? (
+      <h1 className="text-3xl text-white font-bold"> Game Ended ! Your score is {score}</h1>
+      )  : (
+    <Questionaire data={questions[currentIndex]} showAnswers={showAnswers}  handleNextQuestion={handleNextQuestion} handleAnswer={handleAnswer}/>
+    
+    )}
+    </div> 
+    ):(
+    <h1 className="text-2xl text-white font-bold">Wait a moment</h1>
+     );
+}
+
+export default App;
